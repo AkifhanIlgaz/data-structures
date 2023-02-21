c277f4458d495a2f3d769f88af1c1095647da3c8
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-02-21 14:35:44 +0300

    Add queue

---
+++ queue.py
@@ -0,0 +1,16 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.insert(0, item)
+
+    def pop(self):
+        if len(self.items):
+            return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
